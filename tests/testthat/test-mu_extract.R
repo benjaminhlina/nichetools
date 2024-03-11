18ab6da0851573ca0fd21830873eb54598976ffa
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-11 10:26:37 -0400

    Added test to check what the desired output should be

--- tests/testthat/test-mu_extract.R
+++ tests/testthat/test-mu_extract.R
@@ -22,3 +22,22 @@ test_that("error if data isn't a list", {
   )
 
 })
+
+
+test_that("test that the object type and length are correct ", {
+  df_mu_test <- mu_extract(
+    data = niw_fish_post
+  )
+  # check the type returned data frame should be data frame
+  expect_s3_class(object = df_mu_test, class =  "data.frame")
+  # excreted dimensions
+  expected_rows <- 4000
+  expected_cols <- 5
+
+
+  # Check the dimensions using expect_equal
+  expect_equal(nrow(df_mu_test), expected_rows,
+               info = "Number of rows is not as expected.")
+  expect_equal(ncol(df_mu_test), expected_cols,
+               info = "Number of columns is not as expected.")
+})
