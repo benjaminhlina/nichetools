9f1236dbfc94dabf7c395c042e0a49ac718db332
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-11 15:45:13 -0400

    Updated tests to look at column names

--- tests/testthat/test-mu_extract.R
+++ tests/testthat/test-mu_extract.R
@@ -4,7 +4,7 @@ test_that("Test if data object is list ", {
     data = niw_fish_post
   )
 
-expect_type(object = df_mu, type = "list")
+  expect_type(object = df_mu, type = "list")
 
 })
 
@@ -40,4 +40,27 @@ test_that("test that the object type and length are correct ", {
                info = "Number of rows is not as expected.")
   expect_equal(ncol(df_mu_test), expected_cols,
                info = "Number of columns is not as expected.")
+})
+
+
+test_that("Check if column names extracted are correct", {
+
+  expected_names <- c("metric", "sample_name", "sample_number", "d15n", "d13c")
+
+  df_mu_test <- mu_extract(
+    data = niw_fish_post
+  )
+
+  expect_equal(names(df_mu_test), expected_names)
+})
+
+
+test_that("Check if column order", {
+
+  expected_names <- c("metric", "sample_name", "sample_number", "d15n", "d13c")
+
+  df_mu_test_1 <- mu_extract(
+    data = niw_fish_post
+  )
+  expect_identical(names(df_mu_test_1), expected_names)
 })
