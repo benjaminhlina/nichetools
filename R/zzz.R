7fe87aef0bddd7e97708adf8c77a53f3cf918644
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-11 15:43:30 -0400

    Changed startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  packageStartupMessage("version 0.1.0 ('good-dog-colt')")
+  packageStartupMessage("version 0.1.0 ('good-boy-colt')")
   packageStartupMessage("Have you loaded {nicheROVER}? If not, please do so.")
 }
