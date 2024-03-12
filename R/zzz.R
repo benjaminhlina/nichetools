4d6ca968d43435dfcf75b8a821d1eb9fcfbe7659
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-11 20:32:16 -0400

    Updated package startup

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
-.onLoad <- function(libname, pkgname) {
-  packageStartupMessage("version 0.1.0 ('good-boy-colt')")
-  packageStartupMessage("Have you loaded {nicheROVER}? If not, please do so.")
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("version 0.1.0 ('good-boy-colt').\nHave you loaded {nicheROVER}? If not, please do so.")
 }
