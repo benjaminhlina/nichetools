8f33286187fe678f0c0a7afc70f4dbf69ae54b6f
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-13 10:55:24 -0400

    Updated tests to provide full coverage

--- tests/testthat/test-summ-niche_size_extract.R
+++ tests/testthat/test-summ-niche_size_extract.R
@@ -44,3 +44,60 @@ test_that("Parameter 'prob' is set and validated correctly", {
   )
   expect_equal(niche_size_test$niche_size[1], 2.8, tolerance = 0.1)
 })
+
+
+test_that("Check if column names extracted are correct", {
+
+  expected_names <- c("sample_name", "id", "niche_size")
+
+  test_2 <- niche_size_extract(
+    data = niw_fish_post
+  )
+
+  expect_equal(names(test_2), expected_names)
+})
+
+
+test_that("prob errors when given a charcter  or value outside of range", {
+
+  # Test case 3: prob is not numeric, it should raise an error
+  expect_error(niche_size_extract(
+    data = niw_fish_post, prob = "invalid"),
+    "Parameter 'prob' must be a numeric value between 0 and 1.")
+
+  # Test case 4: prob is numeric but outside the valid range,
+  expect_error(niche_size_extract(
+    data = niw_fish_post, prob = 1.5),
+    "Parameter 'prob' must be a numeric value between 0 and 1.")
+}
+)
+
+
+test_that("if name errors if given number", {
+  expect_error(niche_size_extract(
+    data = niw_fish_post,
+    name = 1
+  ), "Argument 'name' must be a character.")
+
+
+})
+
+test_that("output data is the correct size and class", {
+
+  test_3 <- niche_size_extract(
+    data = niw_fish_post
+  )
+
+  # check the type returned data frame should be data frame
+  expect_s3_class(object = test_3, class =  "data.frame")
+  # excreted dimensions
+  expected_rows <- 16000
+  expected_cols <- 3
+
+
+  # Check the dimensions using expect_equal
+  expect_equal(nrow(test_3), expected_rows,
+               info = "Number of rows is not as expected.")
+  expect_equal(ncol(test_3), expected_cols,
+               info = "Number of columns is not as expected.")
+})
