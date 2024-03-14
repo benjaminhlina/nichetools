7d81bfb5e797e3ae3a86b3afc401be5a1c1112b7
benjamin.hlina <benjamin.hlina@gmail.com>
2024-03-14 15:55:58 -0400

    Changed naming conventions as it makes sense to have extract and then the variable so this is overlaps test changes

--- tests/testthat/test-util-extract_overlap.R
+++ tests/testthat/test-util-extract_overlap.R
@@ -1,6 +1,6 @@
 
 test_that("Test if data  is list", {
-  df_over <- overlap_extract(
+  df_over <- extract_overlap(
     data = over_stat
   )
 
@@ -14,7 +14,7 @@ test_that("error if data isn't a array", {
   )
 
   expect_error(
-    overlap_extract(
+    extract_overlap(
       data = dat
     ), regexp = "Input 'data' must be an array."
   )
@@ -22,8 +22,25 @@ test_that("error if data isn't a array", {
 })
 
 test_that("test that the object type and length are correct ", {
-  df_overlap_test <- overlap_extract(
+  df_overlap_test <- extract_overlap(
     data = over_stat
+  )
+  # check the type returned data frame should be data frame
+  expect_s3_class(object = df_overlap_test, class =  "data.frame")
+  # excreted dimensions
+  expected_rows <- 16000
+  expected_cols <- 5
+
+
+  # Check the dimensions using expect_equal
+  expect_equal(nrow(df_overlap_test), expected_rows,
+               info = "Number of rows is not as expected.")
+  expect_equal(ncol(df_overlap_test), expected_cols,
+               info = "Number of columns is not as expected.")
+})
+test_that("test that the object type and length are correct ", {
+  df_overlap_test <- extract_overlap(
+    data = over_stat,
   )
   # check the type returned data frame should be data frame
   expect_s3_class(object = df_overlap_test, class =  "data.frame")
@@ -44,7 +61,7 @@ test_that("Check if column names extracted are correct", {
   expected_names <- c("sample_name_a", "id",
   "sample_name_b", "sample_number", "niche_overlap")
 
-  df_overlap_test_1 <- overlap_extract(
+  df_overlap_test_1 <- extract_overlap(
     data = over_stat
   )
 
@@ -54,7 +71,7 @@ test_that("Check if column names extracted are correct", {
 
 
 test_that("Check if supplying sample_name_a  works", {
-  expect_no_error(df <- overlap_extract(
+  expect_no_error(df <- extract_overlap(
     data = over_stat,
     name_a = "species_a"
   )
@@ -65,7 +82,7 @@ test_that("Check if supplying sample_name_a  works", {
 )
 
 test_that("Check if supplying sample_name_b  works", {
-  expect_no_error(df <- overlap_extract(
+  expect_no_error(df <- extract_overlap(
     data = over_stat,
     name_b = "species_b"
   )
@@ -77,7 +94,7 @@ test_that("Check if supplying sample_name_b  works", {
 
 
 test_that("that supplying both isotope names works ", {
-  expect_no_error(df <- overlap_extract(
+  expect_no_error(df <- extract_overlap(
     data = over_stat,
     name_a = "species_a",
     name_b = "species_b"
@@ -90,13 +107,13 @@ test_that("that supplying both isotope names works ", {
 }
 )
 test_that("that isotope a and b will throw erros if charcter not supplied", {
-  expect_error(df <- overlap_extract(
+  expect_error(df <- extract_overlap(
     data = over_stat,
     name_a = 10,
 
   ), regexp = "The supplied argument for 'name_a' must be a character."
   )
-  expect_error(df <- overlap_extract(
+  expect_error(df <- extract_overlap(
     data = over_stat,
     name_b = 10,
 
