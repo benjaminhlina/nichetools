246353ac4ebf9b12026c6402a796c138cefdb600
benjaminhlina <benjamin.hlina@gmail.com>
2024-03-23 08:39:01 -0400

    Trying to fix global varriable issue

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,16 @@
+
+.pkgenv <- new.env(parent = emptyenv())
+
+.onLoad <- function(libname, pkgname) {
+
+  assign(".sample.name", "sample_name", envir = .pkgglobalenv)
+  assign(".sample.number", "sample_number", envir = .pkgglobalenv)
+  assign(".x_x", "x", envir = .pkgglobalenv)
+  assign(".y_y", "y", envir = .pkgglobalenv)
+  # assign(".sample.name", "sample_name", envir = .pkgglobalenv)
+}
+
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("version 0.1.0 ('good-boy-colt').\nHave you loaded {nicheROVER}? If not, please do so.")
 }
+
