ca4d80da4c884864e5feee9ed82cf0216b5ae45c
benjaminhlina <benjamin.hlina@gmail.com>
2024-03-23 09:01:06 -0400

    Trying to fix global variable issue for pcakge

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,5 @@
-
-.pkgenv <- new.env(parent = emptyenv())
-
-.onLoad <- function(libname, pkgname) {
-
-  assign(".sample.name", "sample_name", envir = .pkgglobalenv)
-  assign(".sample.number", "sample_number", envir = .pkgglobalenv)
-  assign(".x_x", "x", envir = .pkgglobalenv)
-  assign(".y_y", "y", envir = .pkgglobalenv)
-  # assign(".sample.name", "sample_name", envir = .pkgglobalenv)
-}
+utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
+                         "d15n", "d13c"))
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("version 0.1.0 ('good-boy-colt').\nHave you loaded {nicheROVER}? If not, please do so.")
