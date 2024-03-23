671edc9142363875853f6582821ff88c2dcd6fd1
benjaminhlina <benjamin.hlina@gmail.com>
2024-03-23 09:14:09 -0400

    updated global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
 utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
-                         "d15n", "d13c"))
+                         "d15n", "d13c", ".",
+                         "species_a", "species_b", "isotope",
+                         "post_sample", "mu_est", "ellipse_name"))
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("version 0.1.0 ('good-boy-colt').\nHave you loaded {nicheROVER}? If not, please do so.")
