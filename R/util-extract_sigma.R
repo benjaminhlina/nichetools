9de66ff94819e4cba3120cb9a74bb2463eda5d30
benjaminhlina <benjamin.hlina@gmail.com>
2024-04-27 20:38:41 -0400

    Add arguments and method to extract sigma from SIBER objects

--- R/util-extract_sigma.R
+++ R/util-extract_sigma.R
@@ -44,59 +44,86 @@
 #' @export
 
 extract_sigma <-  function(data,
+                           pkg = NULL,
                            isotope_a = NULL,
                            isotope_b = NULL,
-                           format = "wide") {
-  # Check if data is a list
-  if (!inherits(data, "list")) {
-    cli::cli_abort("Input 'data' must be a list.")
+                           data_format = NULL,
+                           data_class = NULL) {
+
+
+  if(pkg == "nicheROVER") {
+    # Check if data is a list
+    if (!inherits(data, "list")) {
+      cli::cli_abort("Input 'data' must be a list.")
+    }
+
+    # defaults of isotpoe a and b
+    if (is.null(isotope_a)) {
+      isotope_a <- "d15n"
+    }
+
+    if (is.null(isotope_b)) {
+      isotope_b <- "d13c"
+    }
+    # Check if isotope_a is character
+    if (!is.character(isotope_a)) {
+      cli::cli_abort("The supplied argument for 'isotope_a' must be a character.")
+    }
+
+    # Check if isotope_b is character
+    if (!is.character(isotope_b)) {
+      cli::cli_abort("The supplied argument for 'isotope_b' must be a character.")
+    }
+
+    # create name vector that will be used to id isotopes.
+    id_isotope <- c(isotope_a, isotope_b)
+    # extract sigma
+    df_sigma <- purrr::map(data, purrr::pluck, 2) |>
+      purrr::imap(~ tibble::as_tibble(.x) |>
+                    dplyr::mutate(
+                      metric = "sigma",
+                      id = id_isotope,
+                      sample_name = .y
+                    )
+      ) |>
+      dplyr::bind_rows() |>
+      tidyr::pivot_longer(cols = -c("id", "sample_name", "metric"),
+                          names_to = "isotope",
+                          values_to = "post_sample"
+      ) |>
+      tidyr::separate(isotope, into = c("isotope", "sample_number"), sep = "\\.")
+
+    if (format %in% "wide") {
+
+      df_sigma <- df_sigma |>
+        tidyr::pivot_wider(names_from = id,
+                           values_from = post_sample)
+      return(df_sigma)
+    }
+    if (format %in% "long") {
+      return(df_sigma)
+
+    }
   }
-
-  # defaults of isotpoe a and b
-  if (is.null(isotope_a)) {
-    isotope_a <- "d15n"
-  }
-
-  if (is.null(isotope_b)) {
-    isotope_b <- "d13c"
-  }
-  # Check if isotope_a is character
-  if (!is.character(isotope_a)) {
-    cli::cli_abort("The supplied argument for 'isotope_a' must be a character.")
-  }
-
-  # Check if isotope_b is character
-  if (!is.character(isotope_b)) {
-    cli::cli_abort("The supplied argument for 'isotope_b' must be a character.")
-  }
-
-  # create name vector that will be used to id isotopes.
-  id_isotope <- c(isotope_a, isotope_b)
-  # extract sigma
-  df_sigma <- purrr::map(data, purrr::pluck, 2) |>
-    purrr::imap(~ tibble::as_tibble(.x) |>
-                  dplyr::mutate(
-                    metric = "sigma",
-                    id = id_isotope,
-                    sample_name = .y
-                  )
-    ) |>
-    dplyr::bind_rows() |>
-    tidyr::pivot_longer(cols = -c("id", "sample_name", "metric"),
-                        names_to = "isotope",
-                        values_to = "post_sample"
-    ) |>
-    tidyr::separate(isotope, into = c("isotope", "sample_number"), sep = "\\.")
-
-  if (format %in% "wide") {
-
-    df_sigma <- df_sigma |>
-      tidyr::pivot_wider(names_from = id,
-                         values_from = post_sample)
+  if (pkg == "SIBER") {
+    if (format == "matrix") {
+    df_sigma <- ellipses.posterior|>
+      purrr::map(~ as.data.frame(.x) |>
+                   dplyr::select("Sigma2[1,1]":"Sigma2[2,2]") |>
+                   mutate(
+                     sample_number = 1:nrow(.),
+                   )) |>
+      bind_rows(.id = "sample_name") |>
+      dplyr::group_split(sample_name, sample_number) |>
+      purrr::map(~ .x |>
+                   dplyr::select("Sigma2[1,1]":"Sigma2[2,2]") |>
+                   matrix(2, 2),
+                 .progress = "Prepare sigma for ellipse"
+      )
     return(df_sigma)
-  }
-  if (format %in% "long") {
-    return(df_sigma)
+    }
+  if (format == "data.frame") {
 
+  }
   }
 }
