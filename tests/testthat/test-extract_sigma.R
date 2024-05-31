d0d05febaaa646a47c1ff0b9a4e96ee0430db077
benjaminhlina <benjamin.hlina@gmail.com>
2024-05-31 10:25:15 -0400

    updated tests for sigma and added test to check if siber objects worked

--- tests/testthat/test-extract_sigma.R
+++ tests/testthat/test-extract_sigma.R
@@ -12,7 +12,6 @@ test_that("test if list", {
   expect_no_error(
     df_sigma_test <- extract_sigma(
       data = niw_fish_post,
-      data_format = "wide"
     )
 
   )
@@ -74,34 +73,6 @@ test_that("test that the object type and length are correct ", {
 
 
 
-
-test_that("Check if supplying isotope_a names works", {
-  expect_no_error(df <- extract_sigma(
-    data = niw_fish_post,
-    isotope_a = "cal_d15n",
-    data_format = "long"
-  )
-  )
-
-  expect_type(object = df$id, type = "character")
-
-  expect_match(object = df$id, regexp = "cal_d15n", all = FALSE)
-
-}
-)
-
-test_that("Check if supplying isotope_b names works", {
-  df <- extract_sigma(
-    data = niw_fish_post,
-    isotope_b = "cal_d13c",
-    data_format = "long"
-
-  )
- expect_type(object = df$id, type = "character")
-
-  expect_match(object = df$id, regexp = "cal_d13c", all = FALSE)
-}
-)
 test_that("that supplying both isotope names works ", {
   df <- extract_sigma(
     data = niw_fish_post,
@@ -133,3 +104,117 @@ test_that("that isotope a and b will throw erros if charcter not supplied", {
 
 }
 )
+
+
+test_that("test if it doesn't error with basic siber object ", {
+  expect_no_error(
+    df_sigma_test <- extract_sigma(
+      data = post_sam_siber,
+      pkg = "SIBER"
+    )
+
+  )
+})
+test_that("test if list", {
+  expect_no_error(
+    df_sigma_test <- extract_sigma(
+      data = post_sam_siber,
+      pkg = "SIBER"
+    )
+
+  )
+  expect_type(object = df_sigma_test, type = "list")
+})
+
+test_that("error if data isn't a list", {
+  dat <- data.frame(
+    x = seq(0, 100, length.out = 10),
+    y = seq(0, 0.1, length.out = 10)
+  )
+
+  expect_error(
+    extract_sigma(
+      data = dat,
+      pkg = "SIBER"
+    ), regexp = "Input 'data' must be a list."
+  )
+
+})
+
+
+
+test_that("test that the object type and length are siber correct ", {
+  df_sigma_test <- extract_sigma(
+    data = post_sam_siber,
+    pkg = "SIBER"
+  )
+  # check the type returned data frame should be data frame
+  expect_s3_class(object = df_sigma_test, class =  "data.frame")
+  # excreted dimensions
+  expected_rows <- 48000
+  expected_cols <- 6
+
+
+  # Check the dimensions using expect_equal
+  expect_equal(nrow(df_sigma_test), expected_rows,
+               info = "Number of rows is not as expected.")
+  expect_equal(ncol(df_sigma_test), expected_cols,
+               info = "Number of columns is not as expected.")
+})
+
+
+test_that("test that the object type and length are correct ", {
+  df_sigma_test <- extract_sigma(
+    data = post_sam_siber,
+    pkg = "SIBER",
+    data_format = "long"
+  )
+  # check the type returned data frame should be data frame
+  expect_s3_class(object = df_sigma_test, class =  "data.frame")
+  # excreted dimensions
+  expected_rows <- 96000
+  expected_cols <- 6
+
+
+  # Check the dimensions using expect_equal
+  expect_equal(nrow(df_sigma_test), expected_rows,
+               info = "Number of rows is not as expected.")
+  expect_equal(ncol(df_sigma_test), expected_cols,
+               info = "Number of columns is not as expected.")
+})
+
+
+
+test_that("that supplying both isotope names works ", {
+  df <- extract_sigma(
+    data = post_sam_siber,
+    pkg = "SIBER",
+    isotope_a = "cal_d15n",
+    isotope_b = "cal_d13c",
+    data_format = "long"
+
+  )
+  expect_type(object = df$id, type = "character")
+
+  expect_match(object = df$id, regexp = "cal_d13c", all = FALSE)
+  expect_match(object = df$id, regexp = "cal_d15n", all = FALSE)
+
+}
+)
+test_that("that isotope a and b will throw erros if charcter not supplied", {
+  expect_error(df <- extract_sigma(
+    data = niw_fish_post,
+    isotope_a = 10,
+
+  ), regexp = "The supplied argument for 'isotope_a' must be a character."
+  )
+  expect_error(df <- extract_sigma(
+    data = niw_fish_post,
+    isotope_b = 10,
+
+  ), regexp = "The supplied argument for 'isotope_b' must be a character"
+  )
+
+}
+)
+
