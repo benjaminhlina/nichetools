f19ec3943fcffd1dfa890d2e8ad02e3755491f88
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-06-06 10:59:46 -0400

    try again

--- R/summ-niche_ellipse.R
+++ R/summ-niche_ellipse.R
@@ -114,8 +114,9 @@ niche_ellipse <- function(
 
   if (random %in% TRUE) {
     if (is.null(set_seed)) {
+      set_seed <- if (!exists(".Random.seed", inherits = TRUE)) NULL else .Random.seed
       # assign(".Random.seed", set_seed)
-      set_seed <- .Random.seed
+      # set_seed <- .Random.seed
       # on.exit({.Random.seed <<- set_seed})
     }
     # if (!is.numeric(set_seed)) {
--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   packageStartupMessage("version 0.2.0 ('spring-running').\nHave you loaded {nicheROVER} or {SIBER}? If not, please do so.")
 }
 utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
-                         "V1", "V2", ".Random.seed",
+                         "V1", "V2",
                          "d15n", "d13c", ".",
                          "species_a", "species_b", "isotope",
                          "post_sample", "mu_est", "ellipse_name"))
