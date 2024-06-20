7369a0ae8ee8f0e31f35c5601acdf78fb31e243e
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-06-20 10:28:14 -0400

    Updated version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("version 0.2.0 ('spring-running').\nHave you loaded {nicheROVER} or {SIBER}? If not, please do so.")
+  packageStartupMessage("version 0.2.1 ('summer-paddling').\nHave you loaded {nicheROVER} or {SIBER}? If not, please do so.")
 }
 utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
                          "V1", "V2",
