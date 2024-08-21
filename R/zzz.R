e3b8193f1170fb0393209507bcbb309a3f77bf26
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-08-21 15:19:05 -0400

    Updated global variables

--- R/zzz.R
+++ R/zzz.R
@@ -5,5 +5,7 @@ utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
                          "V1", "V2",
                          "d15n", "d13c", ".", "community", "group",
                          "community_group", "est",
+                         "c_1", "c_2", "cg_1", "cg_2", "cg_names",
+                         "g_1", "g_2",
                          "species_a", "species_b", "isotope",
                          "post_sample", "mu_est", "ellipse_name"))
