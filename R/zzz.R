88879a6866af0f354df04b3ab32acf0284b85890
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-08-22 11:22:41 -0400

    Add correct global variables

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,11 @@ utils::globalVariables(c("sample_name", "sample_number", "metric", "x", "y",
                          "V1", "V2",
                          "d15n", "d13c", ".", "community", "group",
                          "community_group", "est",
-                         "c_1", "c_2", "cg_1", "cg_2", "cg_names",
-                         "g_1", "g_2",
+                         "c_1", "c_2", "cg_1", "cg_2", "compare",
+                         "g_1", "g_2", "compare_1", "compare_2",
+                         "community_name", "group_name",
+                         "area.1", "area.2", "area1", "area2",
+                         "overlap", "area_1", "area_2",
+
                          "species_a", "species_b", "isotope",
                          "post_sample", "mu_est", "ellipse_name"))
