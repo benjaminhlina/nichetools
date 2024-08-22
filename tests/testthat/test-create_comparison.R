0f607465ae84cfba77017e7f85dd2338cd759edb
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-08-22 11:23:13 -0400

    Added test for create_comparison

---
+++ tests/testthat/test-create_comparison.R
@@ -0,0 +1,61 @@
+library(tibble)
+# Assume that cg_names is a pre-defined dataset similar to the one expected in the function.
+
+# Sample cg_names data for testing
+cg_names <- data.frame(
+  community = rep(c("A", "B"), each = 2),
+  group = rep(c("G1", "G2"), 2)
+)
+
+# Example tests for the create_comparisons function
+test_that("create_comparisons returns correct output for 'within' comparison", {
+  result <- create_comparisons(cg_names, comparison = "within")
+
+  # Expected output
+  expected <- list(
+    `A.G1_A.G2` = tibble(cg_1 = "A.G1", cg_2 = "A.G2"),
+    `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
+    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2"),
+    `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
+
+  )
+
+  expect_equal(result, expected)
+})
+
+test_that("create_comparisons returns correct output for 'among' comparison", {
+  result <- create_comparisons(cg_names, comparison = "among")
+
+  # Expected output
+  expected <- list(
+    `A.G1_B.G1` = tibble(cg_1 = "A.G1", cg_2 = "B.G1"),
+    `A.G2_B.G2` = tibble(cg_1 = "A.G2", cg_2 = "B.G2"),
+    `B.G1_A.G1` = tibble(cg_1 = "B.G1", cg_2 = "A.G1"),
+    `B.G2_A.G2` = tibble(cg_1 = "B.G2", cg_2 = "A.G2")
+  )
+
+  expect_equal(result, expected)
+})
+
+test_that("create_comparisons uses 'within' as default comparison", {
+  result <- create_comparisons(cg_names)
+
+  # Expected output
+  expected <- list(
+    `A.G1_A.G2` = tibble(cg_1 = "A.G1", cg_2 = "A.G2"),
+    `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
+    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2"),
+    `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
+
+  )
+
+  expect_equal(result, expected)
+})
+
+test_that("create_comparisons throws an error for invalid comparison type", {
+  expect_error(
+    create_comparisons(cg_names,
+                       comparison = "invalid"),
+    regexp = "'comparison' must be either 'within' or 'among'.")
+})
+
