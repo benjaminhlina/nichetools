aef8d160d6de13593273588d5b683eacf94037e8
Benjamin Hlina <benjamin.hlina@gmail.com>
2024-08-26 12:07:44 -0400

    Updated tests for create comparison as these now only produce two considering the function filters out duplicates

--- tests/testthat/test-create_comparison.R
+++ tests/testthat/test-create_comparison.R
@@ -16,9 +16,9 @@ test_that("create_comparisons returns correct output for 'within' comparison", {
   # Expected output
   expected <- list(
     `A.G1_A.G2` = tibble(cg_1 = "A.G1", cg_2 = "A.G2"),
-    `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
-    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2"),
-    `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
+    # `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
+    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2")
+    # `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
 
   )
 
@@ -31,9 +31,9 @@ test_that("create_comparisons returns correct output for 'among' comparison", {
   # Expected output
   expected <- list(
     `A.G1_B.G1` = tibble(cg_1 = "A.G1", cg_2 = "B.G1"),
-    `A.G2_B.G2` = tibble(cg_1 = "A.G2", cg_2 = "B.G2"),
-    `B.G1_A.G1` = tibble(cg_1 = "B.G1", cg_2 = "A.G1"),
-    `B.G2_A.G2` = tibble(cg_1 = "B.G2", cg_2 = "A.G2")
+    `A.G2_B.G2` = tibble(cg_1 = "A.G2", cg_2 = "B.G2")
+    # `B.G1_A.G1` = tibble(cg_1 = "B.G1", cg_2 = "A.G1"),
+    # `B.G2_A.G2` = tibble(cg_1 = "B.G2", cg_2 = "A.G2")
   )
 
   expect_equal(result, expected)
@@ -46,9 +46,9 @@ test_that("create_comparisons uses 'within' as default comparison", {
   # Expected output
   expected_1 <- list(
     `A.G1_A.G2` = tibble(cg_1 = "A.G1", cg_2 = "A.G2"),
-    `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
-    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2"),
-    `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
+    # `A.G2_A.G1` = tibble(cg_1 = "A.G2", cg_2 = "A.G1"),
+    `B.G1_B.G2` = tibble(cg_1 = "B.G1", cg_2 = "B.G2")
+    # `B.G2_B.G1` = tibble(cg_1 = "B.G2", cg_2 = "B.G1")
 
   )
 
