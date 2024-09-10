c65be7aad3de07006e3ca9c3360f2ef0fd18e04a
benjaminhlina <benjamin.hlina@gmail.com>
2024-09-10 09:13:46 -0400

    Updated test to have community_df will need to add more tests

--- tests/testthat/test-extract_mu.R
+++ tests/testthat/test-extract_mu.R
@@ -85,11 +85,33 @@ test_that("Check if column order", {
   expect_equal(names(df_mu_test_1), expected_names)
 })
 
+
+library(SIBER)
+
+demo.siber.data.2$group_name <- as.factor(demo.siber.data.2$group)
+
+demo.siber.data.2$group <- as.numeric(demo.siber.data.2$group_name) |>
+  as.character()
+
+demo.siber.data.2$community_name <- as.factor(demo.siber.data.2$community)
+
+demo.siber.data.2$community <- as.numeric(demo.siber.data.2$community_name) |>
+  as.character()
+
+cg_names <- demo.siber.data.2 |>
+  dplyr::distinct(community, group, community_name, group_name)
+
+
+
+
+
+
 # ---- siber ----
 test_that("Test if data object is list ", {
   df_mu <- extract_mu(
     data = post_sam_siber,
-    pkg = "SIBER"
+    pkg = "SIBER",
+    community_df = cg_names
   )
 
   expect_type(object = df_mu, type = "list")
@@ -107,6 +129,8 @@ test_that("error if data isn't a list", {
     extract_mu(
       data = dat,
       pkg = "SIBER",
+      community_df = cg_names
+
     ), regexp = "Input 'data' must be a list."
   )
 
@@ -116,13 +140,14 @@ test_that("error if data isn't a list", {
 test_that("test that the object type and length are correct ", {
   df_mu_test <- extract_mu(
     data = post_sam_siber,
-    pkg = "SIBER"
+    pkg = "SIBER",
+    community_df = cg_names
   )
   # check the type returned data frame should be data frame
   expect_s3_class(object = df_mu_test, class =  "data.frame")
   # excreted dimensions
   expected_rows <- 48000
-  expected_cols <- 5
+  expected_cols <- 9
 
 
   # Check the dimensions using expect_equal
@@ -136,13 +161,14 @@ test_that("test that the object type and length are correct ", {
   df_mu_test <- extract_mu(
     data = post_sam_siber,
     pkg = "SIBER",
+    community_df = cg_names,
     data_format = "wide"
   )
   # check the type returned data frame should be data frame
   expect_s3_class(object = df_mu_test, class =  "data.frame")
   # excreted dimensions
   expected_rows <- 24000
-  expected_cols <- 5
+  expected_cols <- 9
 
 
   # Check the dimensions using expect_equal
@@ -155,12 +181,15 @@ test_that("test that the object type and length are correct ", {
 
 test_that("Check if column names extracted are correct", {
 
-  expected_names <- c("metric", "sample_name", "sample_number", "d13c", "d15n")
+  expected_names <- c("metric",  "community",
+                      "group", "sample_name", "sample_number", "community_name",
+                      "group_name",  "d13c", "d15n")
 
   df_mu_test <- extract_mu(
     data = post_sam_siber,
     pkg = "SIBER",
-    data_format = "wide"
+    data_format = "wide",
+    community_df = cg_names
   )
 
   expect_equal(names(df_mu_test), expected_names)
@@ -169,12 +198,15 @@ test_that("Check if column names extracted are correct", {
 
 test_that("Check if column order", {
 
-  expected_names <- c("metric", "sample_name", "sample_number",
+  expected_names <- c("metric",  "community",
+                      "group", "sample_name", "sample_number", "community_name",
+                      "group_name",
                       "isotope", "mu_est")
 
   df_mu_test_1 <- extract_mu(
     data = post_sam_siber,
-    pkg = "SIBER"
+    pkg = "SIBER",
+    community_df = cg_names
   )
   expect_equal(names(df_mu_test_1), expected_names)
 })
